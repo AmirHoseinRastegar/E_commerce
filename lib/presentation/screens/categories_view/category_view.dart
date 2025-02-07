49cec765d2115dbb97f862b8d1605a9e6e82779b
amirpooyani19 <dota2ker@yahoo.com>
2025-02-07 16:25:11 +0330

    14th commit 19th bahman 1403

--- lib/presentation/screens/categories_view/category_view.dart
+++ lib/presentation/screens/categories_view/category_view.dart
@@ -1,7 +1,6 @@
+import 'package:firebase_e_commerce/data/model/product_model.dart';
 import 'package:firebase_e_commerce/presentation/screens/home/prdocut_details_screen.dart';
-import 'package:firebase_e_commerce/presentation/widgets/loading_screen.dart';
 import 'package:firebase_e_commerce/presentation/widgets/shimmer_loading_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -58,62 +57,7 @@ class _CategoriesViewState extends State<CategoriesView> {
                         itemCount: products.length,
                         itemBuilder: (context, index) {
                           final product = products[index];
-                          return GestureDetector(
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) {
-                                  return ProductDetailsScreen(id: product.id);
-                                },
-                              ));
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(15),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  ClipRRect(
-                                      borderRadius: BorderRadius.circular(15.0),
-                                      child: Image.network(
-                                        product.imageUrl,
-                                        errorBuilder:
-                                            (context, error, stackTrace) =>
-                                                Container(
-                                          decoration: const BoxDecoration(
-                                              color: Colors.red,
-                                              shape: BoxShape.circle),
-                                        ),
-                                      )),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    product.name,
-                                    style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text(
-                                    '\$${product.price}',
-                                    style: const TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.green,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(product.description,
-                                      style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w500))
-                                ],
-                              ),
-                            ),
-                          );
+                          return CategoryWidget(product: product);
                         },
                       ),
                     ));
@@ -123,6 +67,102 @@ class _CategoriesViewState extends State<CategoriesView> {
         }
         return const SizedBox();
       },
+    );
+  }
+}
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({
+    super.key,
+    required this.product,
+  });
+
+  final ProductModel product;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return ProductDetailsScreen(id: product.id);
+          },
+        ));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 15,
+          left: 10,
+          right: 10,
+          bottom: 0,
+        ),
+        child: Card(
+          elevation: 2,
+          child: SizedBox(
+            height: 150,
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.network(
+                      product.imageUrl,
+                      errorBuilder: (context, error, stackTrace) => Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10), // Add some spacing
+                // Wrap the Column in an Expanded widget
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        product.name,
+                        style: const TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        '\$${product.price}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        product.description,
+                        style: const TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
